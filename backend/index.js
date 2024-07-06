890cb4cd7666597dc943c09545738f3249c82254
Binish <fullstack786.dev01@gmail.com>
2024-07-06 09:38:28 +0500

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,17 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 const cors = require('cors')
 const todoRouter = require('./routes/todos.js')
 const dbconnect = require('./db')
-const arr = require('./data.js')
-console.log("🚀 ~ arr:", arr)
 
 
 require('dotenv').config({ path: '.env' })
 
 app.use(express.json())
 app.use(cors())
-// dbconnect()
+dbconnect()
+app.use("/todo", todoRouter)
 // app.use(todoRouter)
 
 app.get("/", (req, res) => res.send("todo server is running on vercel"))
@@ -21,14 +19,11 @@ app.listen(PORT, () => console.log(`Server is Running on: ${PORT} `))
 
 
 
-// app.use("/todo", todoRouter)
-
-
-app.use("/todo", async (req,res)=>{
-console.log("I am in get Todos")
+// app.use("/todo", async (req,res)=>{
+// console.log("I am in get Todos")
     
-    res.send({ message: "all todos", status: 200, data: arr })
-})
+//     res.send({ message: "all todos", status: 200, data: arr })
+// })
 
 // async function getTodos(req, res) {
 //     console.log("I am in get Todos")
@@ -39,7 +34,7 @@ console.log("I am in get Todos")
 
 
 // app.listen(PORT, async () =>{
-//     await dbconnect();
+//     // await dbconnect();
 //     console.log("server is running at 5000")
 //     console.log("db connected")
 // })
